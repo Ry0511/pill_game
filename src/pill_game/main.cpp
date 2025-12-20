cbdbad04e7a1507e2f56791fb60d48361e70c556
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2025-12-20 03:33:40 +0000

    bring stb libraries into the project for rect packing, images, and fonts

--- src/pill_game/main.cpp
+++ src/pill_game/main.cpp
@@ -1,8 +1,16 @@
 #include "pch.h"
 
+#include "vendor/stb_image.h"
+#include "vendor/stb_rect_pack.h"
+#include "vendor/stb_truetype.h"
+
 using namespace pill_game;
 
 int main() {
-    PG_LOG(Info, "Hello {}", "There");
+    stbi_set_flip_vertically_on_load(false);
+    int width, height, channels;
+    stbi_uc* img_data = stbi_load("assets/Enemy_SpriteSheet.png", &width, &height, &channels, 4);
+    PG_LOG(Info, "Enemy_SpriteSheet {}x{}x{}", width, height, channels);
+    stbi_image_free(img_data);
     return 0;
 }
---
+++ src/pill_game/stb_init.cpp
@@ -0,0 +1,18 @@
+//
+// Date       : 19/12/2025
+// Project    : pill_game
+// Author     : -Ry
+//
+
+//
+// Imports the stb libraries and their implementation
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "vendor/stb_image.h"
+
+#define STB_RECT_PACK_IMPLEMENTATION
+#include "vendor/stb_rect_pack.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "vendor/stb_truetype.h"
